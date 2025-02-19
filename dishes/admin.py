43d849edc27461cf989100d564d38fdeed6bfbe6
= <=>
2025-02-18 22:13:08 -0300

    fear: dish

--- dishes/admin.py
+++ dishes/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from dishes.models import Category
+from dishes.models import Category, Dish
 
 @admin.register(Category)
 class CategoryModelAdmin(admin.ModelAdmin):
     list_display = ('name', )
+
+@admin.register(Dish)
+class DishModelAdmin(admin.ModelAdmin):
+    list_display = ('category', 'description', 'name', 'price')
+    search_fields = ('category', 'name')
--- dishes/models.py
+++ dishes/models.py
@@ -9,3 +9,14 @@ class Category(models.Model):
     
     class Meta:
         ordering = ['name']
+
+class Dish(models.Model):
+
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='dish_category')
+    description = models.CharField(max_length=255)
+    name = models.CharField(max_length=20)
+    photo = models.ImageField(upload_to='dishes/')
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
