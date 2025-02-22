0c9538edeaa616da371d47e59e4c5ff003a04649
= <=>
2025-02-21 21:28:30 -0300

    deploy

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# Atualiza o pip
+python -m ensurepip --default-pip
+python -m pip install --upgrade pip
+
+# Instala as dependências do projeto
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
+# Coleta os arquivos estáticos
+python restaurante/manage.py collectstatic --noinput
