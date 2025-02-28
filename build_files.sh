6ba6b16ea30ecdb4bd2ca9a365e4b9d1b83a53c4
Alexsander Sautier <alexsanderesramos@gmail.com>
2025-02-28 13:59:04 -0300

    fixtures

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 echo "Instalando as dependecias"
+python -m venv venv
+source venv/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 
